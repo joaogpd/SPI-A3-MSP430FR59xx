1f6b218b613764f6f3d62f8c42f96cde97cdf3f3
joaogpd <joaogdrumond@aluno.puc-rio.br>
2024-03-19 18:52:48 -0300

    Added SPI implementation, TX and RX tested and functional

---
+++ spi.c
@@ -0,0 +1,163 @@
+#include <spi.h>
+
+void SPI_CS_init(void) {
+    CSCTL0_H = CSKEY >> 8;                    // Unlock CS registers
+    CSCTL1 = DCOFSEL_0;                       // Set DCO to 1MHz
+    CSCTL2 = SELS__DCOCLK | SELM__DCOCLK;     // SMCLK and MCLK source DCO
+    CSCTL3 = DIVS__1 | DIVM__1;               // Set SMCLK and MCLK dividers
+    CSCTL0_H = 0;                             // Lock CS registers
+}
+
+void SPI_GPIO_init(void) {
+    // configure SPI pins, UCA3SIHIGHMO: P6.0; UCA3SOMI: P6.1; UCA3CLK: P6.2
+    GPIO_setAsPeripheralModuleFunctionInputPin(
+        GPIO_PORT_P6,
+        GPIO_PIN0 | GPIO_PIN1 | GPIO_PIN2,
+        GPIO_PRIMARY_MODULE_FUNCTION
+    );
+
+	PMM_unlockLPM5();
+}
+
+void SPI_A3_init(uint8_t clockSource, uint32_t desiredSpiClock) {
+    SPI_GPIO_init();
+    SPI_CS_init();
+
+	EUSCI_A_SPI_initMasterParam masterParams;
+	masterParams.selectClockSource = clockSource;
+	masterParams.clockSourceFrequency = CS_getSMCLK();
+	masterParams.desiredSpiClock = desiredSpiClock;
+	masterParams.msbFirst = EUSCI_A_SPI_MSB_FIRST;
+	masterParams.clockPhase = EUSCI_A_SPI_PHASE_DATA_CHANGED_ONFIRST_CAPTURED_ON_NEXT;
+	masterParams.clockPolarity = EUSCI_A_SPI_CLOCKPOLARITY_INACTIVITY_HIGH;
+	masterParams.spiMode = EUSCI_A_SPI_3PIN;
+
+	EUSCI_A_SPI_initMaster(EUSCI_A3_BASE, &masterParams);	
+
+	EUSCI_A_SPI_enable(EUSCI_A3_BASE);
+
+	EUSCI_A_SPI_clearInterrupt(
+		EUSCI_A3_BASE, 
+		EUSCI_A_SPI_TRANSMIT_INTERRUPT | EUSCI_A_SPI_RECEIVE_INTERRUPT
+	);
+}
+
+void SPI_A3_sendData(uint8_t* sndBuffer, uint8_t sndBufferSize) {
+    EUSCI_A_SPI_enableInterrupt(
+            EUSCI_A3_BASE,
+            EUSCI_A_SPI_TRANSMIT_INTERRUPT
+    );
+
+    uint8_t byteCounter = 0;
+
+	for (byteCounter = 0; byteCounter < sndBufferSize; byteCounter++) {
+	    __bic_SR_register(GIE);
+
+		EUSCI_A_SPI_transmitData(EUSCI_A3_BASE, sndBuffer[byteCounter]);
+
+        __bis_SR_register(LPM0_bits | GIE);
+	}
+
+    while (EUSCI_A_SPI_isBusy(EUSCI_A3_BASE));
+
+    EUSCI_A_SPI_disableInterrupt(
+            EUSCI_A3_BASE,
+            EUSCI_A_SPI_TRANSMIT_INTERRUPT
+    );
+}
+
+void SPI_A3_sendByte(uint8_t byte) {
+    SPI_A3_sendData(&byte, 1);
+}
+
+uint8_t SPI_A3_sendRegisterReceiveByte(uint8_t regAddr) {
+    uint8_t dataByte = 0x55;
+
+    SPI_A3_sendRegisterReceiveData(regAddr, &dataByte, 1);
+
+    return dataByte;
+}
+
+void SPI_A3_sendRegisterReceiveData(uint8_t regAddr, uint8_t* recvBuffer, uint8_t recvBufferSize) {
+    __bic_SR_register(GIE);
+
+    EUSCI_A_SPI_transmitData(EUSCI_A3_BASE, regAddr);
+
+    UCA3IE |= UCRXIE;
+    __bis_SR_register(LPM0_bits | GIE); // wait for RX interrupt
+    volatile uint8_t dummyRX = EUSCI_A_SPI_receiveData(EUSCI_A3_BASE); // ignore received value
+    UCA3IE &= ~(UCRXIE);
+
+    UCA3IE |= UCTXIE;
+    __bis_SR_register(LPM0_bits | GIE); // wait for TX interrupt
+    UCA3IE &= ~(UCTXIE);
+
+    __delay_cycles(40);
+
+    // from now on, dummy bytes will be sent for receiving data
+
+    uint8_t byteCounter = 0;
+
+    for (byteCounter = 0; byteCounter < recvBufferSize; byteCounter++) {
+        EUSCI_A_SPI_transmitData(EUSCI_A3_BASE, 0x00);
+
+        UCA3IE |= UCRXIE;
+        __bis_SR_register(LPM0_bits | GIE);
+        recvBuffer[byteCounter] = EUSCI_A_SPI_receiveData(EUSCI_A3_BASE);
+        UCA3IE &= ~(UCRXIE);
+
+        UCA3IE |= UCTXIE;
+        __bis_SR_register(LPM0_bits | GIE);
+        UCA3IE &= ~(UCTXIE);
+
+        __delay_cycles(40);
+    }
+}
+void SPI_A3_receiveData(uint8_t* recvBuffer, uint8_t recvBufferSize) {
+    __bic_SR_register(GIE);
+
+    EUSCI_A_SPI_enableInterrupt(
+            EUSCI_A3_BASE,
+            EUSCI_A_SPI_RECEIVE_INTERRUPT
+    );
+
+    uint8_t byteCounter = 0;
+
+	for (byteCounter = 0; byteCounter < recvBufferSize; byteCounter++) {
+        __bis_SR_register(LPM0_bits | GIE);
+
+        __bic_SR_register(GIE);
+
+        recvBuffer[recvBufferSize] = EUSCI_A_SPI_receiveData(EUSCI_A3_BASE);
+	}
+
+    EUSCI_A_SPI_disableInterrupt(
+            EUSCI_A3_BASE,
+            EUSCI_A_SPI_RECEIVE_INTERRUPT
+    );
+}
+
+uint8_t SPI_A3_receiveByte(void) {
+    uint8_t dataByte = 0x00;
+
+    SPI_A3_receiveData(&dataByte, 1);
+
+    return dataByte;
+}
+
+void __attribute__ ((interrupt(USCI_A3_VECTOR))) USCI_A3_ISR (void) {
+	switch (__even_in_range(UCA3IV, USCI_SPI_UCTXIFG)) {
+		case USCI_NONE: break;
+		case USCI_SPI_UCRXIFG:
+            UCA3IFG &= ~(UCRXIFG);
+
+            __bic_SR_register_on_exit(LPM0_bits | GIE);
+			break;
+		case USCI_SPI_UCTXIFG:
+		    UCA3IFG &= ~(UCTXIFG);
+
+            __bic_SR_register_on_exit(LPM0_bits | GIE);
+			break;
+		default: break;
+	}
+}
---
+++ spi.h
@@ -0,0 +1,70 @@
+#ifndef SPI_H
+#define SPI_H
+
+#include <msp430.h>
+#include <stdint.h>
+#include <driverlib.h>
+
+/**
+ * Initializes the CS (Clock System) module for SPI operation.
+ * This function should be used with care, as it sets the DCO to 1MHz and
+ * alters the clock sources for SMCLK, MCLK and ACLK, as well as the dividers.
+ * ACLK is left at the default source, while SMCLK and MCLK are sourced from the DCO.
+ */
+void SPI_CS_init(void);
+
+/**
+ * Initializes the SPI A3 GPIO pins. P6.0: UCA3SIMO, P6.1: UCA3SOMI, P6.2: UCA3CLK.
+ * This function also calls unlock_LPM5, so changes are in effect instantly.
+ */
+void SPI_GPIO_init(void);
+
+/**
+ * Initializes the SPI A3 module and enables it.
+ * 
+ * @param clockSource source to be used for the SPI clock. Can be: EUSCI_A_SPI_SMCLK 
+ * or EUSCI_A_SPI_ACLK.
+ * @param desiredSpiClock selects the SPI clock's desired SPI, the clock source that 
+ * was chosen should be taken in mind.
+ */
+void SPI_A3_init(uint8_t clockSource, uint32_t desiredSpiClock);
+
+/** 
+ * Sends a buffer of data through SPI. This call is blocking and will only return when
+ * all the data has been sent. 
+ */
+void SPI_A3_sendData(uint8_t* sndBuffer, uint8_t sndBufferSize);
+
+/**
+ * Sends a byte of data through SPI. This call is blocking and will only return when
+ * the byte has been sent.
+ */
+void SPI_A3_sendByte(uint8_t byte);
+
+/**
+ * Sends a register address through SPI and receives a single byte from slave. This call is
+ * blocking and will only return when the register address has been sent and another received.
+ */
+uint8_t SPI_A3_sendRegisterReceiveByte(uint8_t regAddr);
+
+/**
+ * Sends a register address through SPI and receives a buffer of up to 'recvBufferSize' elements
+ * from the slave. This call is blocking and will only return when the register address has been sent
+ * and all the requested bytes have been received.
+ */
+void SPI_A3_sendRegisterReceiveData(uint8_t regAddr, uint8_t* recvBuffer, uint8_t recvBufferSize);
+
+/** 
+ * Receives a buffer of data through SPI. This call is blocking and will only return when
+ * the byte has been received.
+ */
+void SPI_A3_receiveData(uint8_t* recvBuffer, uint8_t recvBufferSize);
+
+/**
+ * Receives a byte of data through SPI. This call is blocking and will only return when
+ * the byte has been received.
+ */
+uint8_t SPI_A3_receiveByte(void);
+
+
+#endif
